e97f3520511fe9a9c5db9a32556e6e822edf4c9c
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-06-18 09:29:52 -0700

    edits for mobile styling

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -9,9 +9,9 @@ export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
-      <div className="w-full h-full ">
+      <div className="w-full h-full sm:overflow-x-hidden md:overflow-auto">
         <div className="fixed left-0 right-0 flex items-center justify-center ml-auto mr-auto sm:w-[30em] md:w-[70em] -z-50  mt-[25em] rounded-full ">
-          <div className="relative w-full max-w-lg bounds">
+          {/* <div className="relative w-full max-w-lg bounds">
             <div className="absolute top-0 -left-4 w-[20em] h-[20em] bg-indigo-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70  dot"></div>
             <div className="absolute top-0 -right-4 w-[20em] h-[20em] bg-blue-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70   dot2"></div>
             <div className="absolute -bottom-8 left-20 w-[20em] h-[20em] bg-sky-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70   dot3"></div>
@@ -19,7 +19,7 @@ export default function Layout({ preview, children }) {
             <div className="absolute -bottom-[500px] right-[150px] w-[10em] h-[10em] bg-blue-600 rounded-full mix-blend-multiply filter blur-2xl opacity-70   dot2"></div>
             <div className="absolute bottom-[50px] -right-[250px] w-[10em] h-[10em] bg-sky-600 rounded-full mix-blend-multiply filter blur-2xl opacity-70   dot3"></div>
             <div className="relative m-8 space-y-4"></div>
-          </div>
+          </div> */}
         </div>
 
         <Navbar className="w-full" />
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className={"dar"}>
-        <Head />
+        <Head></Head>
         <body className="bg-white dark:bg-slate-700">
           <Main />
           <NextScript />
