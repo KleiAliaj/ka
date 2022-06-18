ed0489188b711dc1cb70bf7ff370f9f3b7260cf9
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-06-18 09:34:27 -0700

    lets try this

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -10,8 +10,8 @@ export default function Layout({ preview, children }) {
     <>
       <Meta />
       <div className="w-full h-full sm:overflow-x-hidden md:overflow-auto">
-        <div className="fixed left-0 right-0 flex items-center justify-center ml-auto mr-auto sm:w-[30em] md:w-[70em] -z-50  mt-[25em] rounded-full ">
-          {/* <div className="relative w-full max-w-lg bounds">
+        {/* <div className="fixed left-0 right-0 flex items-center justify-center ml-auto mr-auto sm:w-[30em] md:w-[70em] -z-50  mt-[25em] rounded-full "> */}
+        {/* <div className="relative w-full max-w-lg bounds">
             <div className="absolute top-0 -left-4 w-[20em] h-[20em] bg-indigo-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70  dot"></div>
             <div className="absolute top-0 -right-4 w-[20em] h-[20em] bg-blue-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70   dot2"></div>
             <div className="absolute -bottom-8 left-20 w-[20em] h-[20em] bg-sky-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70   dot3"></div>
@@ -20,7 +20,7 @@ export default function Layout({ preview, children }) {
             <div className="absolute bottom-[50px] -right-[250px] w-[10em] h-[10em] bg-sky-600 rounded-full mix-blend-multiply filter blur-2xl opacity-70   dot3"></div>
             <div className="relative m-8 space-y-4"></div>
           </div> */}
-        </div>
+        {/* </div> */}
 
         <Navbar className="w-full" />
         {/* <Alert preview={preview} /> */}
