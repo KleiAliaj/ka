461b0db8893ac87202b34b3797e2e465afb07c85
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-07-15 11:13:09 -0700

    lets try that

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
       <Html lang="en" className={"dar"}>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@700&family=Varela+Round&display=swap"
             rel="stylesheet"
