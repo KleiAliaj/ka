1bd75c173b76d10ce678e569a6298a743993723b
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-08-11 09:04:04 -0700

    Updated export path for error

--- pages/_error.js
+++ pages/_error.js
@@ -26,6 +26,7 @@ function Error({ statusCode }) {
   );
 }
 
+export default Error;
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
