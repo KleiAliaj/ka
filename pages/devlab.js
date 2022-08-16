0876ecc673f6dd0200a20cc0a9d45c93f719180a
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-08-16 12:13:28 -0700

    Added 3d scene using react three fiber

--- pages/devlab.js
+++ pages/devlab.js
@@ -1,14 +1,15 @@
+import ScrollThree from "@/components/devlab/scrollThree";
 import ThreeTest from "@/components/devlab/three";
 import React, { useState } from "react";
 
 function DevLab() {
   return (
-    <div className="w-full h-screen page-container">
+    <div className="w-full h-screen page-container !p-0">
       <div className="flex w-full h-full">
         <ThreeTest />
+        {/* <ScrollThree /> */}
       </div>
     </div>
   );
 }
-
 export default DevLab;
