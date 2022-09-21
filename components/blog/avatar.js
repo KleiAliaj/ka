ca40ca57b4adc4421c550773dd1c783d7479e558
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-09-21 14:58:41 -0700

    Updated avatar/date for blog

--- components/blog/avatar.js
+++ components/blog/avatar.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Date from "./date";
 
-export default function Avatar({ name, picture }) {
+export default function Avatar({ name, picture, date }) {
+  console.log(date);
   return (
     <div className="flex items-center">
-      <div className="relative w-12 h-12 mr-4">
+      <div className="relative mr-3 rounded-full shadow-lg h-14 w-14">
         {picture && (
           <Image
             src={`${picture}?auto=format,compress,enhance&w=100&h=100`}
@@ -13,7 +15,10 @@ export default function Avatar({ name, picture }) {
           />
         )}
       </div>
-      <div className="text-2xl font-bold f1">{name}</div>
+      <div className="flex flex-col">
+        <div className="text-2xl font-bold f1">{name}</div>
+        {date && <Date dateString={date} />}
+      </div>
     </div>
   );
 }
--- components/blog/post-header.js
+++ components/blog/post-header.js
@@ -8,13 +8,12 @@ export default function PostHeader({ title, coverImage, date, author, blurb }) {
     <>
       <PostTitle title={title} blurb={blurb} />
       <div className="mb-6 md:block">
-        <div className="text-lg italic text-slate-500 f2 dark:text-slate-300">
-          <Date dateString={date} />
-        </div>
-        {/* <Avatar
+        <div className="text-lg italic text-slate-500 f2 dark:text-slate-300"></div>
+        <Avatar
+          date={date}
           name={author.title}
           picture={author.metadata.picture.imgix_url}
-        /> */}
+        />
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0 ">
         <CoverImage title={title} url={coverImage.imgix_url} />
