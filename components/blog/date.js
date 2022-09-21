50b62ee72cfc29758b3b581cbbb1519025626320
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-09-21 14:58:41 -0700

    Updated avatar/date for blog

--- components/blog/date.js
+++ components/blog/date.js
@@ -1,6 +1,10 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  const date = parseISO(dateString);
+  return (
+    <time className="mt-0 italic f2 opacity-70" dateTime={dateString}>
+      {format(date, "LLLL	d, yyyy")}
+    </time>
+  );
 }
