ea8e080546b9595a563302634a86e8448e353f5b
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-11 09:52:17 -0700

    Cleaned up _app.js to be cleaner and less redundant

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import React from "react";
 import { ThemeProvider } from "next-themes";
 
 import { Toaster } from "react-hot-toast";
-import Head from "next/head";
 import { useRouter } from "next/router";
 import FullLoader from "@/components/etc/FullLoader";
 function MyApp({ Component, pageProps }) {
@@ -32,68 +31,6 @@ function MyApp({ Component, pageProps }) {
   }, [router]);
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <Head>
-        <meta name="description" content="The Blog of Ty Fiero." />
-        <meta
-          name="keywords"
-          content="tyfiero, ty fiero blog, ty fiero, ty blog, code blog"
-        />
-
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#ffffff"></meta>
-        <meta charSet="utf-8" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@FieroTy" />
-        <meta name="twitter:title" content="Ty Fiero" />
-        <meta name="twitter:description" content="The Blog of Ty Fiero." />
-        <meta name="twitter:image" content="./assets/other/Ty-lofi.webp" />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/assets/icons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/assets/icons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/assets/icons/favicon-16x16.png"
-        />
-
-        {/* <!-- Disable tap highlight on IE --> */}
-        <meta name="msapplication-tap-highlight" content="no" />
-
-        {/*Add to homescreen for Chrome on Android --> */}
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="application-name" content="IdeaIsland" />
-        <link
-          rel="icon"
-          sizes="192x192"
-          href="/assets/icons/chrome-touch-icon-192x192.png"
-        />
-
-        {/* Add to homescreen for Safari on iOS --> */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="Ty Fiero" />
-        <link
-          rel="apple-touch-icon"
-          href="/assets/icons/apple-touch-icon.png"
-        />
-
-        {/* <!-- Tile icon for Win8 (144x144 + tile color) --> */}
-        <meta
-          name="msapplication-TileImage"
-          content="/assets/icons/ms-touch-icon-144x144-precomposed.png"
-        />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-      </Head>
       <Layout>
         <Toaster />
         <FullLoader show={loading} />
