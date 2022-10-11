035b69b213a49614aeecaf5bcf6dc87a030c0f97
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-11 09:52:51 -0700

    Added descriptions to 404 and 500 error pages

--- pages/500.js
+++ pages/500.js
@@ -1,27 +1,38 @@
+import Head from "next/head";
 import React from "react";
 
 function Custom500() {
   return (
-    <div className="page-container ">
-      <h2 className="!text-8xl f1 logo ">500</h2>
-      <div className="w-fit h-fit">
-        <div className="flex flex-col items-center ">
-          <p className="text-3xl">An UnGnome error has occurred.</p>
-          <p>The server didn&apos;t like that... Try refreshing maybe? </p>
-          <picture>
-              <source
-                srcSet="/assets/other/gnome.png"
-                type="image/png"
-              />
-          <img
-            src="/assets/other/gnome.png"
-            alt="ungnome error occured"
-            className="h-80 rounded-3xl !m-0 !p-0"
+    <>
+     <Head>
+        <title>500 Error</title>
+        <meta
+            name="description"
+            content={`Website error 500`}
           />
-          </picture>
+      </Head>
+      <div className="page-container ">
+        <h2 className="!text-8xl f1 logo ">500</h2>
+        <div className="w-fit h-fit">
+          <div className="flex flex-col items-center ">
+            <p className="text-3xl">An UnGnome error has occurred.</p>
+            <p>The server didn&apos;t like that... Try refreshing maybe? </p>
+            <picture>
+                <source
+                  srcSet="/assets/other/gnome.png"
+                  type="image/png"
+                />
+            <img
+              src="/assets/other/gnome.png"
+              alt="ungnome error occured"
+              className="h-80 rounded-3xl !m-0 !p-0"
+            />
+            </picture>
+          </div>
         </div>
       </div>
-    </div>
+    </>
+    
   );
 }
 
