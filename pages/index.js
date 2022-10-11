6c12115e3e3a5375cb11fada32e2a4e4477ba4ab
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-11 13:18:56 -0700

    Moved open graph image url to app.js to be dynamic

--- pages/index.js
+++ pages/index.js
@@ -138,7 +138,6 @@ export default function Index({ allPosts }) {
       <Head>
         <title>Ty Fiero&#39;s blog on code, music, and life.</title>
         <meta name="description" content={`Home page for Ty Fiero`} />
-        <link rel="canonical" href="https://www.tyfiero.com" />
       </Head>
       <div className="fade-effect-quick page-container !items-start">
         <DualSection centerText cn="w-full md:mt-10">
