90d76a2bbacf700f24fb87d3dfed6eafb5e15584
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-11 11:22:30 -0700

    Added PWA package

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
   images: {
     domains: ["imgix.cosmicjs.com", "images.unsplash.com", "dl.airtable.com"],
   },
-};
+});
