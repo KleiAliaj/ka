7a8d873933ece11b8443cdfec50db886ec8b392a
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-11 09:52:51 -0700

    Added descriptions to 404 and 500 error pages

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,22 @@
+import Head from "next/head";
 import React from "react";
 
 function FourOhFour() {
   return (
-    <div className="page-container ">
-      <h2 className="!text-8xl f1 logo ">404</h2>
-      <p>The server couldn&apos;t find that page,</p>
-      <p>Much Like I couldn&apos;t find a good 404 joke for this page. oops</p>
-    </div>
+    <>
+     <Head>
+        <title>Not Found</title>
+        <meta
+            name="description"
+            content={`404 not found error`}
+          />
+      </Head>
+      <div className="page-container ">
+        <h2 className="!text-8xl f1 logo ">404</h2>
+        <p>The server couldn&apos;t find that page,</p>
+        <p>Much Like I couldn&apos;t find a good 404 joke for this page. oops</p>
+      </div>
+    </>
   );
 }
 
