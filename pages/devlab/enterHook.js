d3c98093ebb36c1dd0b383910369ecb9d3316281
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-11 13:18:26 -0700

    Added page titlesto niche pages

--- pages/devlab/enterHook.js
+++ pages/devlab/enterHook.js
@@ -1,4 +1,5 @@
 import { useEnter } from "@/lib/hooks/useEnter";
+import Head from "next/head";
 import React from "react";
 
 function EnterHook() {
@@ -26,27 +27,33 @@ function EnterHook() {
   const enter = useEnter(handleHit);
 
   return (
-    <div>
-      <h1 className="heading-lg">EnterHook Test</h1>
-      <div className="flex flex-wrap items-center justify-center w-full h-full gap-5">
-        <input type="text" name="" id="" className="textarea-tw" />
-        <button className="button-1" onClick={handleHit}>
-          Enter to submit
-        </button>
+    <>
+     <Head>
+        <title>Devlab enter</title>
+        <meta name="description" content={`test page`} />
+      </Head>
+      <div>
+        <h1 className="heading-lg">EnterHook Test</h1>
+        <div className="flex flex-wrap items-center justify-center w-full h-full gap-5">
+          <input type="text" name="" id="" className="textarea-tw" />
+          <button className="button-1" onClick={handleHit}>
+            Enter to submit
+          </button>
+        </div>
+        <div
+          className={
+            "mt-20 h-40 flex items-center justify-center   " +
+            (hit ? "bg-sky-400" : "bg-teal-600")
+          }
+        >
+          <p className="text-5xl text-white ">
+            {" "}
+            {hit ? "Enter HIT!!" : "Not hit :("}
+          </p>
+          <p className="ml-10 text-5xl text-white">{hitNum}</p>
+        </div>
       </div>
-      <div
-        className={
-          "mt-20 h-40 flex items-center justify-center   " +
-          (hit ? "bg-sky-400" : "bg-teal-600")
-        }
-      >
-        <p className="text-5xl text-white ">
-          {" "}
-          {hit ? "Enter HIT!!" : "Not hit :("}
-        </p>
-        <p className="ml-10 text-5xl text-white">{hitNum}</p>
-      </div>
-    </div>
+    </>
   );
 }
 
