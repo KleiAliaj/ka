516d3b15bf1ce61612156e37f61b8bb1707b06b1
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-17 16:12:57 -0700

    Added AI section to code page, and moved Oliver to code subdomain

--- pages/code/index.js
+++ pages/code/index.js
@@ -10,6 +10,7 @@ import { motion, useReducedMotion } from "framer-motion";
 import dynamic from "next/dynamic";
 import CompactProjects from "@/components/projects/CompactProjects";
 import Head from "next/head";
+import { experiments } from "@/components/ai/aiArray";
 const CubeWaveSketch = dynamic(
   () => import("../../components/projects/art/CubeWaveSketch"),
   {
@@ -19,6 +20,7 @@ const CubeWaveSketch = dynamic(
 
 function CodeIndex() {
   const prefersReducedMotion = useReducedMotion();
+
   const photoAnimate = {
     offscreen: { scale: prefersReducedMotion ? 1 : 0.6, opacity: 0 },
     onscreen: {
@@ -100,13 +102,13 @@ function CodeIndex() {
       <Head>
         <title>Code and Projects</title>
         <meta
-            name="description"
-            content={"Ty&apos;s code projects and overview of coding career."}
-          />
+          name="description"
+          content={"Ty&apos;s code projects and overview of coding career."}
+        />
       </Head>
       <div className="page-container">
         <h2 className="text-left heading-lg fade-effect-quick ">Code</h2>
-  
+
         <DualSection>
           <TextSection title="" headerClassNames="hidden">
             <motion.h2
@@ -123,10 +125,10 @@ function CodeIndex() {
                 Tailwind CSS, p5.js and Framer Motion, I have the confidence and
                 the know-how to wield these tools to create awesome web
                 experiences for users. <br />
-                <br /> I&apos;m learning Typescript right now and I can tell that
-                it will be a game changer for my workflow. I&apos;d like to learn
-                SQL, machine learning, and backend frameworks down the line, but
-                for now I want to focus on getting good at front end.{" "}
+                <br /> I&apos;m learning Typescript right now and I can tell
+                that it will be a game changer for my workflow. I&apos;d like to
+                learn SQL, machine learning, and backend frameworks down the
+                line, but for now I want to focus on getting good at front end.{" "}
                 <Link href={"/contact"}>
                   <a className="underline text-sky-500">Let me know</a>
                 </Link>{" "}
@@ -145,7 +147,10 @@ function CodeIndex() {
               >
                 Github Profile
                 <picture>
-                  <source srcSet="assets/CodeLogos/github.png" type="image/png" />
+                  <source
+                    srcSet="assets/CodeLogos/github.png"
+                    type="image/png"
+                  />
                   <img
                     src="assets/CodeLogos/github.png"
                     alt="github logo"
@@ -182,7 +187,8 @@ function CodeIndex() {
           </motion.div>
         </DualSection>
         {/* separator */}
-        <div className="md:mb-16 md:mt-16 sm:mb-6 sm:mt-6" />
+        <div className="my-28" />
+
         <DualSection invert>
           <TextSection title="" headerClassNames="hidden">
             <motion.h2
@@ -196,13 +202,13 @@ function CodeIndex() {
                 There&apos;s no better way to learn than by doing. Courses and
                 books are good for grasping the concepts, but applying this
                 knowledge is how to achieve mastery. Each project teaches me
-                something new, not only the technologies themselves, but also new
-                ways of thinking like a programmer.
+                something new, not only the technologies themselves, but also
+                new ways of thinking like a programmer.
                 <br />
                 <br />
                 Side projects encourage exploration, foster curiosity, and have
-                helped me level up as a developer. With each new side project, the
-                complexity and speed increase but the difficulty and strain
+                helped me level up as a developer. With each new side project,
+                the complexity and speed increase but the difficulty and strain
                 decrease. Of all these projects, ideaisland and PriceArt shaped
                 who I am today.
               </p>
@@ -218,7 +224,7 @@ function CodeIndex() {
               </Link>
             </motion.div>
           </TextSection>
-  
+
           <motion.div
             className="flex items-center justify-center w-full h-full mt-11"
             variants={headingAnimate}
@@ -226,7 +232,8 @@ function CodeIndex() {
             <CompactProjects />
           </motion.div>
         </DualSection>
-        <div className="md:mb-16 md:mt-16 sm:mb-6 sm:mt-6" />
+        <div className="my-28" />
+
         <DualSection>
           <TextSection title="" headerClassNames="hidden">
             <motion.h2
@@ -237,10 +244,11 @@ function CodeIndex() {
             </motion.h2>
             <motion.div variants={invertText} className="text-box">
               <p className="font-bold ">
-                Javascript art in P5.js is what made me fall in love with coding.
-                It&apos;s an excellent medium to start learning code, as it helps
-                to visualize how loops, functions, and other concepts work through
-                instant visual feedback. Shout out to Daniel Shiffman of the{" "}
+                Javascript art in P5.js is what made me fall in love with
+                coding. It&apos;s an excellent medium to start learning code, as
+                it helps to visualize how loops, functions, and other concepts
+                work through instant visual feedback. Shout out to Daniel
+                Shiffman of the{" "}
                 <a
                   href="https://thecodingtrain.com/"
                   target="_blank"
@@ -258,16 +266,66 @@ function CodeIndex() {
               variants={invertThird}
               className="flex justify-center w-full mt-5 "
             >
-              <Link href="/code/art">
+              <Link href="/art/generative">
                 <a className="mt-2 text-xl font-bold cursor-pointer button-1 w-fit">
                   All Generative Art <FaArrowRight />
                 </a>
               </Link>
             </motion.div>
           </TextSection>
-  
+
           <motion.div className="w-full" variants={headingAnimate}>
             <CubeWaveSketch />
+          </motion.div>
+        </DualSection>
+        <div className="my-28" />
+
+        <DualSection invert>
+          <TextSection title="" headerClassNames="hidden">
+            <motion.h2
+              variants={invertHeader}
+              className={"font-bold heading-md "}
+            >
+              AI Experiments
+            </motion.h2>
+            <motion.div variants={invertText} className="text-box">
+              <p className="font-bold ">
+                The notion that a computer can learn is fascinating to me. The
+                first time I used GPT3 back in 2020, it was inspiring. It felt
+                like magic, almost anything I&apos;d ask it, it would send back
+                text that made sense, both contextually and grammatically. I
+                spent hours having conversations about philosophy, politics, and
+                technology with a computer that didn&apos; even know my name. It
+                will always be something I&apos;m interested in.
+              </p>
+            </motion.div>
+            {/* <motion.div
+              variants={invertThird}
+              className="flex justify-center w-full mt-5 "
+            >
+              <Link href="/code/projects">
+                <a className="mt-2 text-xl font-bold cursor-pointer button-1 w-fit">
+                  All Projects <FaArrowRight />
+                </a>
+              </Link>
+            </motion.div> */}
+          </TextSection>
+
+          <motion.div
+            className="flex items-center justify-center w-full h-full mt-11"
+            variants={headingAnimate}
+          >
+            <div className="flex flex-wrap justify-center w-full gap-3">
+              {experiments.map((experiment) => (
+                <Experiment
+                  key={experiment.name}
+                  name={experiment.name}
+                  description={experiment.description}
+                  path={experiment.path}
+                  imgSrc={experiment.imgSrc}
+                />
+              ))}
+            </div>
           </motion.div>
         </DualSection>
       </div>
@@ -276,3 +334,26 @@ function CodeIndex() {
 }
 
 export default CodeIndex;
+
+function Experiment({ path, imgSrc, name, description }) {
+  return (
+    <Link href={path}>
+      <a className="flex items-center px-2 py-1 transition sm:w-full md:w-1/3 ring-2 ring-sky-400 hover:ring-4 hover:scale-105 rounded-xl active:scale-95 bg-gradient-to-br from-sky-50 via-sky-100 to-sky-200 dark:from-sky-700 dark:via-sky-800 dark:to-black">
+        <div className="flex flex-col justify-between items-between">
+          <h4 className="heading-sm !text-left">{name}</h4>
+          <p>{description}</p>
+        </div>
+        <div className="flex items-center w-2/3 h-full">
+          <picture>
+            <source srcSet={imgSrc} type="image/png" />
+            <img
+              src={imgSrc}
+              alt="logo"
+              className="object-cover transition duration-500 rounded-lg group-hover:scale-105"
+            />
+          </picture>
+        </div>
+      </a>
+    </Link>
+  );
+}
