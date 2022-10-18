612b4c55135a4d2de7149cf1b6536db3fbea8b09
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-18 15:48:03 -0700

    Cleaned up framer animations for about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -8,44 +8,12 @@ import ImageSection from "@/components/layout/ImageSection";
 import { motion, useReducedMotion } from "framer-motion";
 import Head from "next/head";
 import { BsBook, BsBookshelf } from "react-icons/bs";
+import MotionHeader from "@/components/etc/animation/MotionHeader";
+import MotionText from "@/components/etc/animation/MotionText";
 
 function About() {
   const prefersReducedMotion = useReducedMotion();
 
-  const headingAnimate = {
-    offscreen: { x: prefersReducedMotion ? 0 : -300, opacity: 0 },
-    onscreen: {
-      x: 0,
-      opacity: 1,
-      transition: { type: "spring", bounce: 0.4, duration: 1.5 },
-    },
-  };
-  const textAnimate = {
-    offscreen: {
-      x: prefersReducedMotion ? 0 : -300,
-      opacity: 0,
-      scale: prefersReducedMotion ? 1 : 0.2,
-    },
-    onscreen: {
-      x: 0,
-      opacity: 1,
-      scale: 1,
-      transition: { type: "spring", bounce: 0.2, duration: 1.5, delay: 0.2 },
-    },
-  };
-  const thirdAnimate = {
-    offscreen: {
-      x: prefersReducedMotion ? 0 : -300,
-      opacity: 0,
-      scale: prefersReducedMotion ? 1 : 0.2,
-    },
-    onscreen: {
-      x: 0,
-      opacity: 1,
-      scale: 1,
-      transition: { type: "spring", bounce: 0.2, duration: 1.5, delay: 0.4 },
-    },
-  };
   return (
     <>
       <Head>
@@ -56,47 +24,45 @@ function About() {
         />
       </Head>
       <div className="page-container">
-        <h2 className="text-left heading-lg ">About Me</h2>
+        <h2 className="text-left heading-lg fade-effect-quick ">About Me</h2>
 
         <DualSection centerText>
           <TextSection>
-            <motion.h2
-              variants={headingAnimate}
-              className={"font-bold heading-md "}
-            >
-              Who am I?
-            </motion.h2>
-            <motion.div variants={textAnimate} className="w-full text-box">
-              <p className="font-bold text-sky-600">
-                I have always been a science geek, I love learning how the world
-                works and thinking about how it might work better. Initially, I
-                was fascinated with the natural sciences and biology, which led
-                me to getting a B.S. in Microbiology with a minor in Zoology
-                from Colorado State University. While I still enjoy biology, I
-                made a decision to pivot my career to pursue computer science
-                for more opportunities to express my creativity and to build
-                useful things. This scientific background has given me a unique
-                lens on computer science. Follow along on my journey to see what
-                I create next!
-              </p>
-            </motion.div>
-            <motion.div
-              variants={thirdAnimate}
-              className="flex justify-center w-full"
-            >
-              <div className="flex gap-3">
-                <Link href="/about/links">
-                  <a className="mt-10 text-xl font-bold w-fit button-1">
-                    Best Links <FaLink />
-                  </a>
-                </Link>
-                <Link href="/about/books">
-                  <a className="mt-10 text-xl font-bold w-fit button-1">
-                    Books <BsBook />
-                  </a>
-                </Link>
+            <MotionHeader>
+              <h2 className={"font-bold heading-md "}>Who am I?</h2>
+            </MotionHeader>
+            <MotionText>
+              <div className="w-full text-box">
+                <p className="font-bold text-sky-600">
+                  I have always been a science geek, I love learning how the
+                  world works and thinking about how it might work better.
+                  Initially, I was fascinated with the natural sciences and
+                  biology, which led me to getting a B.S. in Microbiology with a
+                  minor in Zoology from Colorado State University. While I still
+                  enjoy biology, I made a decision to pivot my career to pursue
+                  computer science for more opportunities to express my
+                  creativity and to build useful things. This scientific
+                  background has given me a unique lens on computer science.
+                  Follow along on my journey to see what I create next!
+                </p>
               </div>
-            </motion.div>
+            </MotionText>
+            <MotionText delay={0.2}>
+              <div className="flex justify-center w-full">
+                <div className="flex gap-3">
+                  <Link href="/about/links">
+                    <a className="mt-10 text-xl font-bold w-fit button-1">
+                      Best Links <FaLink />
+                    </a>
+                  </Link>
+                  <Link href="/about/books">
+                    <a className="mt-10 text-xl font-bold w-fit button-1">
+                      Books <BsBook />
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </MotionText>
           </TextSection>
           <ImageSection
             src="/assets/other/profileshot.webp"
