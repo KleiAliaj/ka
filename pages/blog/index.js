760e1bc0b87099f97a75300aafe90319eb66fd93
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-18 11:51:37 -0700

    Enhanced contact and blog page for mobile

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -17,35 +17,34 @@ export default function Index({ allPosts }) {
       <Head>
         <title>Ty Fiero&apos;s blog posts on code, music, and life.</title>
         <meta
-            name="description"
-            content={"All blog posts written by Ty Fiero."}
-          />
+          name="description"
+          content={"All blog posts written by Ty Fiero."}
+        />
       </Head>
-      <Container>
-        {/* <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8 f1">
+
+      {/* <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8 f1">
           Blog
         </h1> */}
-        <div className="page-container">
-          <h1 className="text-left heading-lg ">Blog</h1>
+      <div className="page-container">
+        <h1 className="text-left heading-lg ">Blog</h1>
 
-          <CustomForm />
+        <CustomForm />
 
-          <h3 className="mt-10 mb-4 text-2xl f1 text-sky-800 dark:text-sky-300">
-            Latest Post:
-          </h3>
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.metadata.cover_image}
-              date={heroPost.created_at}
-              author={heroPost.metadata.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.metadata.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </div>
-      </Container>
+        <h3 className="mt-10 mb-4 text-2xl f1 text-sky-800 dark:text-sky-300">
+          Latest Post:
+        </h3>
+        {heroPost && (
+          <HeroPost
+            title={heroPost.title}
+            coverImage={heroPost.metadata.cover_image}
+            date={heroPost.created_at}
+            author={heroPost.metadata.author}
+            slug={heroPost.slug}
+            excerpt={heroPost.metadata.excerpt}
+          />
+        )}
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </div>
     </>
   );
 }
