b0ed78255700c9cf5d140ff0e8a288cffa32c047
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-18 11:04:10 -0700

    Modifed home page to be more mobile friendly

--- components/projects/CompactProjects.js
+++ components/projects/CompactProjects.js
@@ -5,13 +5,13 @@ import { slimProjects } from "@/components/projects/projectArray";
 function CompactProjects() {
   return (
     <>
-      <div className="flex flex-wrap justify-center w-full gap-4 md:h-96 xl:h-72">
+      <div className="flex flex-wrap justify-center gap-4 md:w-full sm:w-screen h-fit">
         {slimProjects.map((project, index) => {
           return (
             <Link href={project.route} key={index}>
               <a
                 className={
-                  "  rounded-xl transition pr-3 hover:scale-105 active:scale-95 flex items-center gap-3 shadow-md  hover:shadow-lg md:w-64 sm:w-[13.2rem] h-fit " +
+                  "  rounded-xl transition md:pr-3 hover:scale-105 active:scale-95 flex items-center sm:gap-1 lg:gap-3 shadow-md  hover:shadow-lg lg:w-64 sm:w-[10.8rem] h-fit !break-words " +
                   project.styles.shadow
                 }
               >
@@ -28,12 +28,15 @@ function CompactProjects() {
                 <div className="flex flex-col ">
                   <p
                     className={
-                      project.styles.textColor + " sm:text-base md:text-xl f1"
+                      project.styles.textColor +
+                      " sm:text-base sm:whitespace-pre-line  lg:text-xl f1"
                     }
                   >
                     {project.title}
                   </p>
-                  <p className="-mt-1 text-xs ">{project.shortDescription}</p>
+                  <p className="-mt-1 text-xs sm:hidden lg:block">
+                    {project.shortDescription}
+                  </p>
                 </div>
               </a>
             </Link>
