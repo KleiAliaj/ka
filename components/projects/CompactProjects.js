c6e74aae4253a28c56526850e8022d7cfed1b1a8
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-21 09:37:38 -0700

    Made footer text larger, and removed anchor tag bug

--- components/projects/CompactProjects.js
+++ components/projects/CompactProjects.js
@@ -10,51 +10,53 @@ function CompactProjects() {
         {slimProjects.map((project, index) => {
           return (
             <Link href={project.route} key={index}>
-              <Atropos
-                shadow={false}
-                activeOffset={40}
-                className={
-                  "rounded-xl transition md:pr-3  active:scale-95  shadow-md cursor-pointer hover:shadow-lg lg:w-64 sm:w-[10.8rem] h-fit  " +
-                  project.styles.shadow
-                }
-              >
-                <a
+              <a>
+                <Atropos
+                  shadow={false}
+                  activeOffset={40}
                   className={
-                    "   flex items-center rounded-xl sm:gap-1 lg:gap-3   w-full h-fit !break-words "
+                    "rounded-xl transition md:pr-3  active:scale-95  shadow-md cursor-pointer hover:shadow-lg lg:w-64 sm:w-[10.8rem] h-fit  " +
+                    project.styles.shadow
                   }
                 >
                   <div
-                    className="w-16 h-16 sm:min-w-[4rem]  shadow-md rounded-xl overflow-hidden "
-                    data-atropos-offset="1"
+                    className={
+                      "   flex items-center rounded-xl sm:gap-1 lg:gap-3   w-full h-fit !break-words "
+                    }
                   >
-                    <picture>
-                      <source srcSet={project.imgSrc} type="image/webp" />
-                      <img
-                        src={project.imgSrc}
-                        alt={"Image of the " + project.title + " project"}
-                        className="object-cover rounded-xl"
-                      />
-                    </picture>
-                  </div>
-                  <div className="flex flex-col ">
-                    <p
-                      className={
-                        project.styles.textColor +
-                        " sm:text-base sm:whitespace-pre-line  lg:text-xl f1"
-                      }
-                      data-atropos-offset="-10"
+                    <div
+                      className="w-16 h-16 sm:min-w-[4rem]  shadow-md rounded-xl overflow-hidden "
+                      data-atropos-offset="1"
                     >
-                      {project.title}
-                    </p>
-                    <p
-                      className="-mt-1 text-xs sm:hidden lg:block"
-                      data-atropos-offset="-5"
-                    >
-                      {project.shortDescription}
-                    </p>
+                      <picture>
+                        <source srcSet={project.imgSrc} type="image/webp" />
+                        <img
+                          src={project.imgSrc}
+                          alt={"Image of the " + project.title + " project"}
+                          className="object-cover rounded-xl"
+                        />
+                      </picture>
+                    </div>
+                    <div className="flex flex-col ">
+                      <p
+                        className={
+                          project.styles.textColor +
+                          " sm:text-base sm:whitespace-pre-line  lg:text-xl f1"
+                        }
+                        data-atropos-offset="-10"
+                      >
+                        {project.title}
+                      </p>
+                      <p
+                        className="-mt-1 text-xs sm:hidden lg:block"
+                        data-atropos-offset="-5"
+                      >
+                        {project.shortDescription}
+                      </p>
+                    </div>
                   </div>
-                </a>
-              </Atropos>
+                </Atropos>
+              </a>
             </Link>
           );
         })}
