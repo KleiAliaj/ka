0e6b0b08ae3008065e226ab6ff2de1edc2318af7
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-10-26 07:16:17 -0700

    Refactored ai experiments

--- components/ai/aiArray.js
+++ components/ai/aiArray.js
@@ -5,6 +5,13 @@ export const experiments = [
       "GPT-3 powered life advice character that takes on various topics.",
     path: "/code/ai/Oliver",
     imgSrc: "/assets/other/projects/oliver/OliThumb.png",
+    styles: {
+      color: "bg-red-400",
+      bColor: "ring-red-500 ",
+      textColor: " text-red-600 dark:text-red-400",
+      shadow:
+        "!shadow-red-500 bg-gradient-to-br from-white via-white to-red-200 dark:from-slate-900 dark:via-slate-900 dark:to-red-900",
+    },
   },
   {
     name: "AI Artwork",
@@ -12,5 +19,12 @@ export const experiments = [
       "Images I've made using DALLE-2, MidJourney, and Stable Diffusion.",
     path: "/art/ai-art",
     imgSrc: "/assets/other/projects/art/aiImgs/aigrid.webp",
+    styles: {
+      color: "bg-blue-400",
+      bColor: "ring-blue-500 ",
+      textColor: " text-blue-600 dark:text-blue-400",
+      shadow:
+        "!shadow-blue-500 bg-gradient-to-br from-white via-white to-blue-200 dark:from-slate-900 dark:via-slate-900 dark:to-blue-900",
+    },
   },
 ];
