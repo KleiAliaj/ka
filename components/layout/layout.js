e5c777cc7ca590606926c5303f47a669d350dbd0
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-11-06 15:21:14 -0800

    Next/font addition

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -4,10 +4,15 @@ import "lazysizes";
 import "lazysizes/plugins/parent-fit/ls.parent-fit";
 import Navbar from "./navbar";
 
-export default function Layout({ preview, children }) {
+export default function Layout({ preview, children, fontCn }) {
   return (
     <>
-      <div className="w-full h-full min-h-screen sm:overflow-x-hidden md:overflow-auto">
+      <div
+        className={
+          "w-full h-full min-h-screen sm:overflow-x-hidden md:overflow-auto " +
+          fontCn
+        }
+      >
         <div className="fixed top-0 left-0 background -z-50">
           {/* Inspiration from https://codepen.io/Mamboleoo/pen/BxMQYQ */}
           <span></span>
