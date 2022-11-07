4fc0f1a6ad398324fa2b4eafb776807811484ee4
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-11-07 12:51:00 -0800

    Updated about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -31,15 +31,15 @@ function About() {
         <DualSection>
           <TextSection cn="lg:min-w-[36rem]">
             <MotionHeader>
-              <h2 className={"font-bold heading-md "}>Who I am</h2>
-              {/* <h2 className={"font-bold heading-md "}>The Short</h2> */}
+              {/* <h2 className={"font-bold heading-md "}>Who I am</h2> */}
+              <h2 className={"font-bold heading-md "}>The Short</h2>
             </MotionHeader>
             <MotionText>
               <div className="w-full text-box">
                 <p>
                   I&apos;m a former Microbiologist turned Web Developer striving
                   for world-class user experiences through simplicity and clean
-                  design. When I&apos;m not working I&apos;m making{" "}
+                  design. When I&apos;m not working, I&apos;m making{" "}
                   <Link className="anc" href="/art">
                     art
                   </Link>{" "}
@@ -57,105 +57,115 @@ function About() {
                 </p>
               </div>
             </MotionText>
-            {/* <MotionHeader>
+            <MotionHeader>
               <h2 className={"font-bold heading-md mt-5 "}>The Long</h2>
             </MotionHeader>
             <MotionText>
               <div className="w-full text-box">
-                <MotionHeader>
+                <MotionHeader delay={0.2}>
                   <h2 className={"font-bold heading-sm text-left "}>
-                    My Story
+                    My Journey
                   </h2>
+                  {/* <h2 className={"font-bold heading-sm text-left "}>
+                    What I&apos;ve Done
+                  </h2> */}
+                  <p>
+                    I grew up in sunny San Diego for much of my life, before
+                    trading the sun for snow to move to Denver with my family.
+                    In high school, I developed my interest in all things
+                    science, which led me to major in Microbiology at{" "}
+                    <a
+                      className="anc italic"
+                      href="https://www.colostate.edu/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      CSU,
+                    </a>{" "}
+                    where I got my Bachelor&apos;s degree. In college, I was a
+                    Research Assistant studying the black plague bacteria in
+                    amoebas, as well as the evolution of the skeletons of frogs
+                    and toads. Research on the latter{" "}
+                    <a
+                      className="anc"
+                      href="https://pubmed.ncbi.nlm.nih.gov/29383712/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      was published
+                    </a>{" "}
+                    in{" "}
+                    <a
+                      className="anc italic"
+                      href="https://onlinelibrary.wiley.com/journal/15585646"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Evolution
+                    </a>{" "}
+                    journal. I also worked as a computer lab monitor and a
+                    librarian, where I taught the homeless how to use computers,
+                    and helped over a dozen get jobs in the community by working
+                    with them on their resumes and doing mock interviews. (
+                    <span className="italic">
+                      I was also nearly stabbed twice, but you win some and you
+                      lose some
+                    </span>
+                    )
+                    <br />
+                    <br />
+                    After College, I worked at the mighty{" "}
+                    <a
+                      className="anc"
+                      href="https://www.newbelgium.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      New Belgium
+                    </a>{" "}
+                    brewery as a Microbiologist, QA testing beer and tending to
+                    all the little yeast babies. Eventually, I could no longer
+                    handle the snow and ice of Colorado and decided to move to
+                    rainy Seattle with my best friend. My last job in the
+                    biology field was monitoring and preventing bioterrorism in
+                    Washington state as a Microbiologist at the Department of
+                    Health. (
+                    <span className="italic">
+                      Wild stories there, but I can&apos;t tell you any of them
+                    </span>{" "}
+                    😉). After eight years in the biology field, I decided to
+                    switch things up and teach myself web development in June of
+                    2021. Many factors weighed into that decision, arthritis
+                    from pipetting being one, but most importantly I was not
+                    able to express my creativity as often as I&apos;d liked. It
+                    proved to be a fantastic decision, and I&apos;ve loved every
+                    minute of my learning thus far.
+                  </p>
                 </MotionHeader>
-                <p>
-                  Born in San Diego, moved to Denver at 10, then went to College
-                  at{" "}
-                  <a
-                    className="anc italic"
-                    href="https://www.colostate.edu/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    CSU
-                  </a>{" "}
-                  to get a Bachelor&apos;s degree in Microbiology. Moved to
-                  Seattle three weeks before the Covid-19 lockdown in 2020.
-                </p>
-                <MotionHeader delay={0.4}>
+                {/* <MotionHeader delay={0.4}>
                   <h2 className={"font-bold heading-sm text-left  mt-2"}>
                     What I Do Now
                   </h2>
+                  <p>Currently teaching myself web development.</p>
                 </MotionHeader>
-                <p>Currently teaching myself web development.</p>
-                <MotionHeader delay={0.2}>
-                  <h2 className={"font-bold heading-sm text-left mt-2"}>
-                    What I Used To Do
-                  </h2>
-                </MotionHeader>
-                <p>
-                  Worked as a computer lab monitor and a librarian, where I
-                  helped the homeless get jobs by teaching them how to make
-                  their own resumes. (And was almost stabbed twice, you win some
-                  you lose some lol.)
-                  <br />
-                  In College, I was a Research Assistant (glorified lab rat)
-                  studying the black plague in amoebas, as well as the evolution
-                  of the skeletons of frogs and toads. Research on the latter{" "}
-                  <a
-                    className="anc"
-                    href="https://pubmed.ncbi.nlm.nih.gov/29383712/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    was published
-                  </a>{" "}
-                  in{" "}
-                  <a
-                    className="anc italic"
-                    href="https://onlinelibrary.wiley.com/journal/15585646"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Evolution
-                  </a>{" "}
-                  journal.
-                  <br />
-                  After College, I worked at{" "}
-                  <a
-                    className="anc italic"
-                    href="https://www.newbelgium.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    New Belgium
-                  </a>{" "}
-                  brewery as a Microbiologist, QA testing beer and tending to
-                  all the little yeasty babies.
-                  <br />
-                  My last job in the biology field was monitoring and preventing
-                  bioterrorism as a Microbiologist at the Department of Health.
-                  Wild stories, can&apos;t tell you any of them ;)
-                </p>
-
-                <MotionHeader delay={0.6}>
+                <MotionHeader delay={0.4}>
                   <h2 className={"font-bold heading-sm text-left  mt-2"}>
-                    Interests
+                    Where I&apos;m Headed
                   </h2>
+                  <p>Currently teaching myself web development.</p>
+                </MotionHeader> */}
+                <MotionHeader delay={0.6}>
+                  <p className="mt-10 text-center">
+                    Enough about me,
+                    <span className="font-bold"> how can I help you?</span>{" "}
+                    <Link className="anc" href="/contact">
+                      Shoot me a message
+                    </Link>
+                    , let&apos;s work together!
+                  </p>
                 </MotionHeader>
-                <p>
-                  I love code, AI, nature, reptiles, plants, photography, art
-                  and my dogs.
-                  <br />
-                  <br />
-                  Enough about me,
-                  <span className="font-bold"> how can I help you?</span>{" "}
-                  <Link className="anc" href="/contact">
-                   Shoot me a message
-                  </Link>
-                  , let&apos;s work together!
-                </p>
               </div>
-            </MotionText> */}
+            </MotionText>
           </TextSection>
 
           <div className="w-1/2 flex flex-col items-center ">
