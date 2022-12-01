d173b0b9c1bd5c7ffa700fa10f5e664af77a2a69
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-11-30 22:33:59 -0800

    Added new dev page for playing with twitter pics

---
+++ pages/devlab/diagram.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import React from "react";
+
+function Diagram() {
+  return (
+    <>
+      <Head>
+        <title>Diagram visuals</title>
+        <meta name="description" content={`Visuals for diagram posts`} />
+      </Head>
+      <div className="page-container !px-0 h-full min-h-screen !py-0 mt-28">
+        <div className="flex flex-col items-center justify-center h-full">
+          {/* <section className="w-[1200px] h-[675px] ring-4 ring-white rounded-xl bg-white relative overflow-hidden flex justify-center items-center"> */}
+          {/* <img
+            src="/assets/other/sky.png"
+            alt="gradient bg"
+            className="w-[1200px] h-auto absolute top-0 left-0 object-contain opacity-75 z-0"
+          /> */}
+
+          {/* <div className="bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-xl overflow-hidden w-[89%] h-[90%] shadow-lg z-10 diagram-box"></div> */}
+          {/* <div className="bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-xl overflow-hidden w-[89%] h-[90%] z-10 diagram-box"></div> */}
+
+          <div className="  z-10 diagram-anim group  flex justify-center items-center">
+            <div className=" diagram-main p-3">
+              <img
+                src="/assets/other/ex.png"
+                alt="ven diagram"
+                className="h-[100%] w-auto  object-contain opacity-75 z-0 group-hover:opacity-100 opacity-80 transition duration-500 ease-in-out scale-75 group-hover:scale-100 translate-z-10"
+              />
+            </div>
+          </div>
+
+          {/* </section> */}
+        </div>
+        <div className=" w-full h-full mt-48">
+          {" "}
+          <div className="testbox bg-white mx-auto">Hello world</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Diagram;
